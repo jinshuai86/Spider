81e2daed0b87a74692ae2a292d4fd57335e03e28
JinShuai <jinshuai789@gmail.com>
2018-03-29 19:28:40 +0800

    per thread per jedis

--- src/main/java/com/jinshuai/utils/JedisUtils.java
+++ src/main/java/com/jinshuai/utils/JedisUtils.java
@@ -44,6 +44,8 @@ public class JedisUtils {
             jedisPoolConfig.setMaxTotal(JedisPoolConfig.MAX_ACTIVE);
             jedisPoolConfig.setMaxIdle(JedisPoolConfig.MAX_IDLE);
             jedisPoolConfig.setMaxWaitMillis(JedisPoolConfig.MAX_WAIT);
+            //jedisPoolConfig.setTestOnBorrow(true);
+            jedisPoolConfig.setTestOnReturn(true);
             jedisPool = new JedisPool(jedisPoolConfig, ip, port, JedisPoolConfig.TIMEOUT);
         } else {
             jedisPool = maps.get(ip);
@@ -66,22 +68,22 @@ class JedisPoolConfig {
      * 可用连接实例的最大数目，默认值为8；
      * 如果赋值为-1，则表示不限制；如果pool已经分配了maxActive个jedis实例，则此时pool的状态为exhausted(耗尽)。
      */
-    public static int MAX_ACTIVE = 5000;
+    public static int MAX_ACTIVE = 2048;
 
     /**
      * 控制一个pool最多有多少个状态为idle(空闲的)的jedis实例，默认值也是8。
      */
-    public static int MAX_IDLE = 8;
+    public static int MAX_IDLE = 200;
 
     /**
      * 等待可用连接的最大时间，单位毫秒，默认值为-1，表示永不超时。如果超过等待时间，则直接抛出JedisConnectionException；
      * */
-    public static int MAX_WAIT = 15000;
+    public static int MAX_WAIT = 10;
 
     /**
      * 超时时间
      * */
-    public static int TIMEOUT = 10000;
+    public static int TIMEOUT = 10;
 
     /**
      * 重试次数
