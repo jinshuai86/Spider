af63bf04322483e1e3c1d7c21bd0c1de3df4ef23
JinShuai <jinshuai789@gmail.com>
2018-03-29 19:29:13 +0800

    bug fix

--- src/main/java/com/jinshuai/core/downloader/impl/HttpClientPoolDownloader.java
+++ src/main/java/com/jinshuai/core/downloader/impl/HttpClientPoolDownloader.java
@@ -4,6 +4,10 @@ import com.jinshuai.core.downloader.Downloader;
 import com.jinshuai.entity.Page;
 import com.jinshuai.entity.UrlSeed;
 import com.jinshuai.utils.HttpUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author: JS
@@ -13,10 +17,20 @@ import com.jinshuai.utils.HttpUtils;
  */
 public class HttpClientPoolDownloader implements Downloader {
 
+    private final Logger LOGGER = LoggerFactory.getLogger(HttpClientPoolDownloader.class);
+
     public Page download(UrlSeed urlSeed) {
-        String html = HttpUtils.getSingleInstance().getContent(urlSeed.getUrl());
-        Page page = new Page(urlSeed, html);
-        return page;
+        Page page = null;
+        try {
+            String html = HttpUtils.getSingleInstance().getContent(urlSeed.getUrl());
+            Document document = Jsoup.parse(html, urlSeed.getUrl());
+            page = new Page(urlSeed, document);
+            return page;
+        } catch (Exception e) {
+            LOGGER.error("下载器处的解析器解析相应文本出错 " + e.getMessage());
+        } finally {
+            return page;
+        }
     }
 
 }
