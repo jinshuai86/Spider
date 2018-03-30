10e81a8a7a540131eba5cb3576034447ce1ce0d9
JinShuai <jinshuai789@gmail.com>
2018-03-30 08:50:36 +0800

    config Jedis

--- src/main/java/com/jinshuai/utils/JedisUtils.java
+++ src/main/java/com/jinshuai/utils/JedisUtils.java
@@ -78,12 +78,12 @@ class JedisPoolConfig {
     /**
      * 等待可用连接的最大时间，单位毫秒，默认值为-1，表示永不超时。如果超过等待时间，则直接抛出JedisConnectionException；
      * */
-    public static int MAX_WAIT = 10;
+    public static int MAX_WAIT = 10000;
 
     /**
      * 超时时间
      * */
-    public static int TIMEOUT = 10;
+    public static int TIMEOUT = 10000;
 
     /**
      * 重试次数
