3c321ac6565d0b11291a25f98e44d0537300da5d
jinshuai86 <jinshuai789@gmail.com>
2019-04-04 15:51:47 +0800

    bug fix

--- src/main/java/com/jinshuai/Spider.java
+++ src/main/java/com/jinshuai/Spider.java
@@ -24,28 +24,20 @@ import java.util.concurrent.*;
  */
 public class Spider {
 
-    /**
-     * 设置爬虫的各个组件：downloader，parser，saver，scheduler，urlSeed
-     * 配置线程池
-     * 启动
-     */
-    private static final int TARGET_TASK_NUMBER = 800;
     private static final Logger LOGGER = LoggerFactory.getLogger(Spider.class);
 
-    private static Spider build() {
-        return new Spider()
-                .setDownloader(new HttpClientPoolDownloader())
-                .setParser(new NewsParser())
-                .setSaver(new TextSaver())
-//                .setScheduler(new RedisScheduler())
-                .setScheduler(new PriorityQueueScheduler())
-                .setThreadPool();
-    }
-
+    /**
+     * 设置爬虫组件：scheduler、downloader、parser、saver、
+     */
     private Downloader downloader;
     private Parser parser;
     private Saver saver;
     private Scheduler scheduler;
+
+    /**
+     * 目标任务量
+     * */
+    private int targetTaskNumbers = 800;
 
     /**
      * 线程池参数配置
@@ -54,16 +46,21 @@ public class Spider {
     private static final int CORE_POOL_SIZE = 2;
     private static final int MAX_POOL_SIZE = 5;
     private static final long KEEP_ALIVE_TIME = 1500L;
-
-    /**
-     * 任务队列size
-     */
     private static final int MAX_QUEUE_SIZE = 100;
 
     /**
      * 最多只有MAX_QUEUE_SIZE + MAX_POOL_SIZE个任务并发执行 -> 控制任务的提交速率
      */
     private Semaphore semaphore = new Semaphore(MAX_QUEUE_SIZE + MAX_POOL_SIZE);
+
+    private Spider setScheduler(Scheduler scheduler) {
+        if (scheduler == null) {
+            LOGGER.error("未设置调度器，启动失败");
+            return null;
+        }
+        this.scheduler = scheduler;
+        return this;
+    }
 
     private Spider setDownloader(Downloader downloader) {
         if (downloader == null) {
@@ -92,12 +89,10 @@ public class Spider {
         return this;
     }
 
-    private Spider setScheduler(Scheduler scheduler) {
-        if (scheduler == null) {
-            LOGGER.error("未设置调度器，启动失败");
-            return null;
-        }
-        this.scheduler = scheduler;
+    private Spider setThreadPool() {
+        pool = new ThreadPoolExecutor(CORE_POOL_SIZE, MAX_POOL_SIZE, KEEP_ALIVE_TIME, TimeUnit.MILLISECONDS,
+                new LinkedBlockingQueue<>(MAX_QUEUE_SIZE));
+
         return this;
     }
 
@@ -110,10 +105,12 @@ public class Spider {
         return this;
     }
 
-    private Spider setThreadPool() {
-        pool = new ThreadPoolExecutor(CORE_POOL_SIZE, MAX_POOL_SIZE, KEEP_ALIVE_TIME, TimeUnit.MILLISECONDS,
-                new LinkedBlockingQueue<>(MAX_QUEUE_SIZE));
-
+    private Spider setTargetTaskNumbers(int targetTaskNumbers) {
+        if (targetTaskNumbers < 0) {
+            LOGGER.error("无效的目标任务数量:[{}]",targetTaskNumbers);
+            return null;
+        }
+        this.targetTaskNumbers = targetTaskNumbers;
         return this;
     }
 
@@ -122,27 +119,24 @@ public class Spider {
         UrlSeed urlSeed = null;
         while (true) {
             try {
-                LOGGER.info("已完成任务数量:[{}]，运行中线程数量：[{}]，最大线程运行数量: [{}]，工作队列任务数量：[{}]",
-                        pool.getCompletedTaskCount(), pool.getActiveCount(), pool.getMaximumPoolSize(), pool.getQueue().size());
-                urlSeed = scheduler.pop();
                 // 种子仓库没有种子并且活跃线程为0(不再解析页面产生新的种子)
-                if (urlSeed == null && pool.getActiveCount() == 0) {
+                if ((urlSeed = scheduler.pop()) == null && pool.getActiveCount() == 0) {
                     pool.shutdown();
                     LOGGER.info("解析完毕，正在停止......");
                     break;
-                } else if (urlSeed == null) { // 线程池中的线程还在完成任务，需要等待
+                } else if (urlSeed == null) {
                     LOGGER.info("种子仓库已无种子，等待中......");
                     Thread.sleep(1000);
                 } else {
                     // 一切正常，分配线程处理任务，
-                    LOGGER.info("正在处理:[{}]，优先级(默认5):[{}]", urlSeed.getUrl(), urlSeed.getPriority());
+                    LOGGER.info("准备解析URL:[{}]，优先级(默认5):[{}]", urlSeed.getUrl(), urlSeed.getPriority());
                     // 获取许可
                     semaphore.acquire();
                     // 调用线程执行任务
                     pool.execute(new SpiderWork(urlSeed));
                 }
                 // 自定义的任务停止目标: 当完成的任务数量达到800 TODO:随便写的--..
-                if (pool.getCompletedTaskCount() >= TARGET_TASK_NUMBER && urlSeed == null && pool.getQueue().size() == 0) {
+                if (pool.getCompletedTaskCount() >= targetTaskNumbers && urlSeed == null && pool.getQueue().size() == 0) {
                     pool.shutdown();
                     LOGGER.info("达到目标，正在停止......");
                 }
@@ -150,9 +144,6 @@ public class Spider {
                 LOGGER.error("当前线程被中断", ie);
             } catch (RejectedExecutionException ree) {
                 LOGGER.error("拒绝此次提交的任务[{}]", urlSeed, ree);
-            } finally {
-                // 释放许可
-                semaphore.release();
             }
         }
     }
@@ -175,10 +166,19 @@ public class Spider {
                 page.getUrlSeeds().forEach(seed -> scheduler.push(seed));
                 saver.save(page);
             } finally {
-                // 完成任务，释放许可
                 semaphore.release();
             }
         }
+    }
+
+    private static Spider build() {
+        return new Spider()
+                .setDownloader(new HttpClientPoolDownloader())
+                .setParser(new NewsParser())
+                .setSaver(new TextSaver())
+//                .setScheduler(new RedisScheduler())
+                .setScheduler(new PriorityQueueScheduler())
+                .setThreadPool();
     }
 
     /**
@@ -194,6 +194,7 @@ public class Spider {
     public static void main(String[] args) {
         Spider.build()
                 .addUrlSeed(new UrlSeed("http://xww.hebut.edu.cn/gdyw/index.htm", 5))
+                .setTargetTaskNumbers(100)
                 .run();
     }
 
